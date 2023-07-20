f8b8ac4d4a847eef045511be2f00082aae920c17
spaziu <spaziutempu@protonmail.ch>
2023-07-20 22:55:00 +0200

    adding title and favicon

--- src/App/Components/NavBar.tsx
+++ src/App/Components/NavBar.tsx
@@ -10,7 +10,9 @@ export default function NavBar() {
   return (
     <>
       <nav className="sticky top-0 z-10 flex h-12 items-center justify-center border-b-2 border-slate-800 bg-slate-800 text-blue-300/75 shadow-lg transition md:top-2 md:mx-4 md:rounded-xl xl:top-[1.5vh] xl:h-fit xl:min-h-[97vh] xl:flex-col xl:px-6 xl:py-4">
-        <MdMovie className="text-3xl text-red-600 xl:text-4xl" />
+        <div className="ml-4 text-2xl xl:mx-auto xl:mb-8">
+          <MdMovie className="text-3xl text-red-600 xl:text-4xl" />
+        </div>
         <ul className="mx-auto flex items-center justify-center gap-x-3 md:gap-x-4 xl:mx-0 xl:mb-auto xl:flex-col xl:gap-y-6">
           <NavLink to="/" title="Home">
             <RiFunctionFill className="text-2xl transition hover:scale-125 hover:text-blue-200/95 active:animate-ping xl:text-3xl" />
